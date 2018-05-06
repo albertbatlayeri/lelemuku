80fc9d34e769979b1525be4b1cd83e6514ed8921
albertbatlayeri <39035564+albertbatlayeri@users.noreply.github.com>
2018-05-06 21:25:53 +0000

    randomv
    randomv

---
+++ randomvideos.js
@@ -0,0 +1,15 @@
+var videos = [
+'dKp4P1oPqAY',
+'G_Xsl5ZSwqQ',
+'bFPGoI_6qJo',
+'5TaeLqQaPyM',
+'AgV8782kQ3I',
+'2ENQEuUOmuA',
+'Jk9rh7LBEc8',
+'9dC9TComymQ','iRqgP2C2h7o
+','EWxevt0MrwI'];
+
+var index=Math.floor(Math.random() * videos.length);
+var html='<iframe style="padding: 16px 0;height:250px;" src="https://www.youtube.com/embed/' + videos[index] + '" allowfullscreen></iframe>';
+document.write(html);
+
