c45bd63f9080a26595993c78ddab3b929ebef78e
albertbatlayeri <39035564+albertbatlayeri@users.noreply.github.com>
2020-12-21 19:07:10 +0900

    Add files via upload

---
+++ flexible-post-6-1a.js
@@ -0,0 +1,20 @@
+<b:if cond='data:view.isSingleItem'>
+<div id='disqus_thread'/>
+<script>
+var disqus_shortname = 'dirga-id-1';
+var disqus_blogger_current_url = &quot;<data:blog.canonicalUrl/>&quot;;
+if (!disqus_blogger_current_url.length) {
+disqus_blogger_current_url = &quot;<data:blog.url/>&quot;;
+}
+var disqus_blogger_homepage_url = &quot;<data:blog.homepageUrl/>&quot;;
+var disqus_blogger_canonical_homepage_url = &quot;<data:blog.canonicalHomepageUrl/>&quot;;
+//<![CDATA[
+(function() {
+var bloggerjs = document.createElement('script');
+bloggerjs.type = 'text/javascript';
+bloggerjs.async = true;
+bloggerjs.src = '//'+disqus_shortname+'.disqus.com/blogger_item.js';
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bloggerjs);
+})();
+//]]> </script>
+</b:if>
